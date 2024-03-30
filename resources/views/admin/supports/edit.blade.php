b48c893c0ca758d352590569208c6e915c74a155
Dorivaldo Ngoma <dorivaldongoma@gmail.com>
2024-03-30 04:06:29 +0100

    chore: organization of views with components

--- resources/views/admin/supports/edit.blade.php
+++ resources/views/admin/supports/edit.blade.php
@@ -1,15 +1,8 @@
 <h1>Dúvida {{ $support->id }}</h1>
 
-@if($errors->any())
-    @foreach($errors->all() as $error)
-        {{ $error }}
-    @endforeach
-@endif
+<x-alert></x-alert>
 
 <form action="{{ route('supports.update', $support->id) }}" method="POST">
-    @csrf
     @method('PUT')
-    <input type="text" name="subject" placeholder="Assunto" value="{{ $support->subject }}">
-    <textarea name="body" cols="30" rows="10" placeholder="Descrição">{{ $support->body }}</textarea>
-    <button type="submit">Enviar</button>
+    @include('admin.supports.partials.form', compact('support'))
 </form>
